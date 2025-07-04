db8f9c424ab6e4ccb57d4a400f6c13f3f7dd6bc2
mikeg5hgp <mike@mikegtech.com>
2025-07-04 06:57:38 -0500

    fix build error.

---
+++ AssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Reflection;
+
+namespace M3.Net.Modules.Transcripts.Application;
+
+public static class AssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+}
